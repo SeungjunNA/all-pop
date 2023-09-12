5fd94ef9a31ded141f3b9a14a9b7e949b826f504
SeungjunNA <nsjjsng1@gmail.com>
2023-09-13 01:37:39 +0900

    프로젝트 생성

---
+++ allpop/src/test/java/backend/allpop/AllpopApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.allpop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllpopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
